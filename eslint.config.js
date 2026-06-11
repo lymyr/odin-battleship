c60f7b6679f1d186de66a95bac99832113ac1433
lymyr <176290600+lymyr@users.noreply.github.com>
2026-06-11 19:04:13 +0800

    added tests directory to globalIgnores

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,6 @@ import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["dist/*"]),
+  globalIgnores(["dist/*", "tests/*"]),
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
 ]);
