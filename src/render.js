e875c45b1b9e57348e86fb21cc494bf97688bcad
lymyr <176290600+lymyr@users.noreply.github.com>
2026-06-11 21:04:21 +0800

    shortened render board code
    removed helper.js as i feel like it would fit better in render.js anyway

--- src/render.js
+++ src/render.js
@@ -1,49 +1,43 @@
 import Ship from "./ships.js";
-import Helper from "./helper.js";
 import { p1, p2 } from "./gameController.js";
 
 class Render {
   static render() {
     let size = 40;
 
-    this.boardP1(size);
-    this.boardP2(size);
+    this.boards(size)
   }
 
-  static boardP1(size = 40) {
-    const board = document.querySelector("player1 > board");
-    board.innerHTML = ""
-    p1.board.board.forEach((boardCell) => {
-      const cell = document.createElement("cell");
-      if (boardCell[2] instanceof Ship) cell.setAttribute("class", "ship");
-      cell.setAttribute("style", `height: ${size}px; width: ${size}px`);
-      board.appendChild(cell);
-      
-      Helper.addHitListener(p1, boardCell, cell)
-    });
+  static boards(size) {
+    const playerList = [p1, p2]
+    for (let i = 0; i < playerList.length; i++) {
+        const board = document.querySelector(`player${i+1} > board`)
+        board.innerHTML = ""
 
-    board.setAttribute(
-      "style",
-      `display:grid; grid-template-columns: repeat(10, ${size}px); grid-template-rows: (10, ${size}px);`,
-    );
-  }
+        playerList[i].board.board.forEach(boardCell => {
+            const cell = document.createElement("cell");
+            if (boardCell[2] instanceof Ship) cell.setAttribute("class", "ship");
+            cell.setAttribute("style", `height: ${size}px; width: ${size}px`);
+            board.appendChild(cell);
+            board.setAttribute(
+            "style",
+            `display:grid; grid-template-columns: repeat(10, ${size}px); grid-template-rows: (10, ${size}px);`,
+            );
 
-  static boardP2(size = 40) {
-    const board = document.querySelector("player2 > board");
-    board.innerHTML = ""
-    p2.board.board.forEach((boardCell) => {
-      const cell = document.createElement("cell");
-      if (boardCell[2] instanceof Ship) cell.setAttribute("class", "ship");
-      cell.setAttribute("style", `height: ${size}px; width: ${size}px`);
-      board.appendChild(cell);
-    
-      Helper.addHitListener(p2, boardCell, cell)
-    });
+            if (boardCell[3] == true) {
+                cell.addEventListener("click", () => {
+                    playerList[i].board.receiveAttack([boardCell[0], boardCell[1]]);
+                    this.render();
+                });
+            }
+            else
+                cell.classList.add("hit");
 
-    board.setAttribute(
-      "style",
-      `display:grid; grid-template-columns: repeat(10, ${size}px); grid-template-rows: (10, ${size}px);`,
-    );
+            if (boardCell[2] != null && boardCell[2].isSunk()) {
+                cell.classList.add("sunk");
+            }
+        })
+    }
   }
 }
 
