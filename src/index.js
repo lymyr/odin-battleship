02407604a43a1c99ae7c9370c97624748f61b478
lymyr <176290600+lymyr@users.noreply.github.com>
2026-06-12 20:54:35 +0800

    style: polished ui elements
    finished with project tho only with randomizer :<

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ import Player from "./logic/Player.js";
 import Boards from "./components/Boards.js";
 import Prompt from "./components/Prompt.js";
 
-
 // preplaced for now
 const p1 = new Player(true);
 const p2 = new Player();
 
-Helper.randomizeBoard(p1)
-Helper.randomizeBoard(p2)
+Helper.randomizeBoard(p1);
+Helper.randomizeBoard(p2);
 
-Boards.render()
-Prompt.renderButton("Start Game!")
-Boards.addRandomizeButton(p1)
+Boards.render();
+Prompt.renderButton("Start Game!");
+Boards.addRandomizeButton(p1);
 
-export {p1, p2}
+export { p1, p2 };
